22b2c7060f3b0658080e8121a4d2eb84e4558db5
Pi Delport <pjdelport@gmail.com>
2022-01-12 11:25:40 +0200

    chore: update Rust SGX SDK revision for 1.1.4 follow-up fixes

--- prepare-1.1.4-intel-2.15.1.sh
+++ prepare-1.1.4-intel-2.15.1.sh
@@ -7,7 +7,8 @@
 export INTEL_SGX_SDK_INSTALLER_URL="https://download.01.org/intel-sgx/sgx-linux/2.15.1/distro/ubuntu20.04-server/sgx_linux_x64_sdk_2.15.101.1.bin"
 
 # Rust SGX SDK 1.1.4
-# Merge of 1.1.4-testing branch. See: https://github.com/apache/incubator-teaclave-sgx-sdk/issues/360
-export RUST_SGX_SDK_REVISION="863378876c55025084572e22554bbedd57eead97"
+# Merge of 1.1.4-testing branch, plus follow-up fixes.
+# See: https://github.com/apache/incubator-teaclave-sgx-sdk/issues/360
+export RUST_SGX_SDK_REVISION="565960cd7b4b36d1188459d75652619971c43f7e"
 
 "$(dirname "$0")"/prepare.sh
